42f90d7c394518cae92e2cac73f515b0035db722
Sara <saragholampour971@yahoo.com>
2025-09-27 12:23:07 +0330

    fix react query provider

--- components/react-query/ReactQueryProvider.tsx
+++ components/react-query/ReactQueryProvider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {ReactNode, useState} from "react";
+import {ReactNode} from "react";
 import {DehydratedState, HydrationBoundary, QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {usePathname, useRouter} from "next/navigation";
 
@@ -8,36 +8,34 @@ interface Props {
   dehydratedState?: DehydratedState;
 }
 
+let queryClient: QueryClient;
+
 export default function ReactQueryProvider({children, dehydratedState}: Props) {
   const router = useRouter();
   const pathname = usePathname();
-  const [queryClient] = useState(() => new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 5 * 60 * 1000,
-        retry: false,
+  if (!queryClient) {
+    queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 5 * 60 * 1000,
+          retry: false,
+        },
+
+        mutations: {
+          retry: false,
+          onError: (error) => {
+            if (error.message == 'Unauthorized')
+              router.push(`/login?redirect=${encodeURIComponent(pathname)}`);
+            else {
+              console.error(error.message, error)
+            }
+
+          },
+
+        }
       },
-
-      mutations: {
-        retry: false,
-        onMutate: variables => {
-          console.log(variables, 'variables');
-        },
-
-        onError: (error) => {
-          console.log('error from react query', error);
-          console.log('pathname', pathname);
-          if (error.message == 'Unauthorized')
-            router.push(`/login?redirect=${encodeURIComponent(pathname)}`);
-          else {
-            console.error(error.message, error)
-          }
-
-        },
-
-      }
-    },
-  }));
+    })
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -46,41 +44,3 @@ export default function ReactQueryProvider({children, dehydratedState}: Props) {
     </QueryClientProvider>
   );
 }
-
-// // ReactQueryProvider.tsx
-// "use client";
-//
-// import {PropsWithChildren, useState} from "react";
-// import {DehydratedState, HydrationBoundary, QueryClient, QueryClientProvider,} from "@tanstack/react-query";
-//
-// export default function ReactQueryProvider({
-//                                              children,
-//                                              dehydratedState,
-//                                            }: PropsWithChildren<{ dehydratedState?: DehydratedState }>) {
-//   const [queryClient] = useState(() => {
-//     return new QueryClient({
-//       // defaultOptions: {
-//       //   mutations: {
-//       //     onError: (error: Error) => {
-//       //       console.log(error, '?<><><', error.name, error.message, error.cause);
-//       //     },
-//       //
-//       //   },
-//       //   queries: {
-//       //     staleTime: 60 * 1000,
-//       //     retry: false, // برای جلوگیری از retry خودکار روی 401
-//       //
-//       //   },
-//       // },
-//     });
-//   });
-//
-//
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <HydrationBoundary state={dehydratedState}>
-//         {children}
-//       </HydrationBoundary>
-//     </QueryClientProvider>
-//   );
-// }
